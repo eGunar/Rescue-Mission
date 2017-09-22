b835001590dd4f73e8edbc42f5fed33e36860fd9
eGunar <egunnar1999@gmail.com>
2017-09-22 23:34:03 +0200

    change title

--- Rescue Mission/Src/main.cpp
+++ Rescue Mission/Src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
 	game = new Game();
 
-	game->init("First", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	game->init("Rescue Mission", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game->running())
 	{
